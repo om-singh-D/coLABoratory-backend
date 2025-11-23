a767c4ee7224e84ff05d82e51f060b9b9281e6ab
echo <omsingh2849@gmail.com>
2025-11-23 23:13:37 +0530

    initial push minor fixes

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from 'express';
 import morgan from 'morgan';
 const app = express();
 
+
+import connectDB from './db/db.js'; 
+
+connectDB();
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
