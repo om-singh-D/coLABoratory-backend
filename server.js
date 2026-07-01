53ad0f4f1581c16077d606de43e1d6ae778e7513
echo <omsingh2849@gmail.com>
2026-07-01 12:54:32 +0530

    feat: integrate socket.io into server to enable real-time bidirectional communication

--- server.js
+++ server.js
@@ -2,10 +2,30 @@ import 'dotenv/config';
 
 import http from 'http';
 import app from './app.js';
+import { Server } from 'socket.io';
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*' // Allow all origins for now, you can restrict this later
+  }
+});
+
+
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('event', data => {
+    /* … */
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
